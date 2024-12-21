f0eb5ddc3fc087cc0c1f34cfa731487606939b05
SushmitaSPatil <sushmitapatil389@gmail.com>
2024-12-21 10:26:38 +0530

    Add files via upload

---
+++ sim.py
@@ -0,0 +1,12 @@
+fruit=['apple','orange','mango','cherry']
+i=len(fruit)-1 
+
+while i>=0:
+    print(fruit[i])
+    i-=1
+
+another_stock=['soap','shampoo','brush','cleaning']
+i=len(another_stock)-1
+while i>=0:
+    print(another_stock[i])
+    i -=1
